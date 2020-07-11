fe38d4b124d825ad6c67957d026793d293ad42aa
fncheng <dong862647669@gmail.com>
2020-07-11 11:08:41 +0800

    update: 掘金去app下载

--- src/zhuanlan-zhihu.js
+++ src/zhuanlan-zhihu.js
@@ -2,6 +2,7 @@
 // @name        知乎专栏去下载App
 // @namespace   Violentmonkey Scripts
 // @match       http*://zhuanlan.zhihu.com/p/*
+// @match       http*://juejin.im/post/*
 // @grant       none
 // @version     1.0
 // @author      fncheng
@@ -13,8 +14,12 @@
   "use strict";
   function removeAppDownload() {
     var app = document.getElementsByClassName("OpenInAppButton")[0];
-    if(app){
+    var app1 = document.getElementsByClassName("open-in-app")[0];
+    if (app) {
       app.style.display = "none";
+    }
+    if (app1) {
+      app1.style.display = "none";
     }
   }
   removeAppDownload();
